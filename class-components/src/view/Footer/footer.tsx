5fc79b261506e89002927627ca2ad532e6a39fd0
Darya Pchelintseva <raelin@mail.ru>
2024-07-05 10:28:11 +0300

    fix: fix anchor in footer

--- class-components/src/view/Footer/footer.tsx
+++ class-components/src/view/Footer/footer.tsx
@@ -6,7 +6,7 @@ class Footer extends Component {
     return (
       <footer>
         <div className="footer-inner">
-          <a href="https://github.com/raenlin/">
+          <a href="https://github.com/raenlin/" target="blank">
             <h3>raenlin</h3>
           </a>
         </div>
