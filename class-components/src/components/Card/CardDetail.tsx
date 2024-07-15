372244220fb7c62fa825b7675295f9f3629844cb
Darya Pchelintseva <raelin@mail.ru>
2024-07-15 16:34:01 +0500

    feat: add loader to detail page

--- class-components/src/components/Card/CardDetail.tsx
+++ class-components/src/components/Card/CardDetail.tsx
@@ -7,6 +7,8 @@ import { fetchData } from '../../services/api';
 function CardDetails() {
   const { id } = useParams<{ id: string }>();
   const [items, setItems] = useState<Planet[]>([]);
+  const [error, setError] = useState<Error | null>(null);
+  const [fetching, setFetching] = useState(false);
   const navigate = useNavigate();
 
   const handleClick = () => {
@@ -16,8 +18,17 @@ function CardDetails() {
   useEffect(() => {
     const fetchItem = async () => {
       if (id) {
-        const data = await fetchData(id);
-        setItems(data);
+        try {
+          setFetching(true);
+          const data = await fetchData(id);
+          setItems(data);
+          setError(null);
+        } catch (error) {
+          setItems([]);
+          setError(error as Error);
+        } finally {
+          setFetching(false);
+        }
       }
     };
     fetchItem();
@@ -27,28 +38,38 @@ function CardDetails() {
     return;
   }
 
+  if (error) {
+    return <p>Error {error.message}</p>;
+  }
+
   return (
-    <div className="card-details">
-      {items.map((item) => (
-        <div key={item.name}>
-          <h2>{item.name}</h2>
-          <div>
-            <p>Diameter: {item.diameter}</p>
-            <p>Rotation-period: {item.rotation_period}</p>
-            <p>Orbital-period: {item.orbital_period}</p>
-            <p>Climate: {item.climate}</p>
-            <p>Terrain: {item.terrain}</p>
-            <p>Gravity: {item.gravity}</p>
-            <p>Population: {item.population}</p>
-            <p>Surface-water: {item.surface_water}</p>
-            <p>Created: {item.created}</p>
-          </div>
+    <>
+      {fetching ? (
+        <div className="loader"></div>
+      ) : (
+        <div className="card-details">
+          {items.map((item) => (
+            <div key={item.name}>
+              <h2>{item.name}</h2>
+              <div>
+                <p>Diameter: {item.diameter}</p>
+                <p>Rotation-period: {item.rotation_period}</p>
+                <p>Orbital-period: {item.orbital_period}</p>
+                <p>Climate: {item.climate}</p>
+                <p>Terrain: {item.terrain}</p>
+                <p>Gravity: {item.gravity}</p>
+                <p>Population: {item.population}</p>
+                <p>Surface-water: {item.surface_water}</p>
+                <p>Created: {item.created}</p>
+              </div>
+            </div>
+          ))}
+          <button onClick={handleClick} className="card-details-close">
+            Close
+          </button>
         </div>
-      ))}
-      <button onClick={handleClick} className="card-details-close">
-        Close
-      </button>
-    </div>
+      )}
+    </>
   );
 }
 
