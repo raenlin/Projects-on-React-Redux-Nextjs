c358309d5ac1b484569f43af3cd162764bb7d238
Darya Pchelintseva <raelin@mail.ru>
2024-07-16 10:26:30 +0500

    feat: add redux

---
+++ class-components/src/store/appSlice.ts
@@ -0,0 +1,9 @@
+// import { createSlice } from '@reduxjs/toolkit';
+
+// const appSlice = createSlice({
+//   name: 'planets',
+//   initialState: {
+//     planets: []
+//   },
+//   reducers
+// })
